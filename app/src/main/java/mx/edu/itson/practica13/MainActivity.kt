618f6d307d50f374d07c3debbbf49fc983721829
itzelhgarza <itzel.garza216628@potros.itson.edu.mx>
2023-05-02 21:17:02 -0700

    Practica 13 completa pero no funciona el botón

---
+++ app/src/main/java/mx/edu/itson/practica13/MainActivity.kt
@@ -0,0 +1,50 @@
+package mx.edu.itson.practica13
+
+import android.content.ContentValues.TAG
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import mx.edu.itson.practica13.databinding.ActivitySignInBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivitySignInBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+    }
+
+    /*private fun SignIn(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    reaload()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    //updateUI(null)
+                }
+            }
+    }
+
+    private fun reaload() {
+        val intent = Intent(this, MainActivity::class.java)
+        this.startActivity(intent)
+    }*/
+}
---
+++ app/src/main/java/mx/edu/itson/practica13/SignInActivity.kt
@@ -0,0 +1,62 @@
+package mx.edu.itson.practica13
+
+import android.content.ContentValues.TAG
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import mx.edu.itson.practica13.databinding.ActivitySignInBinding
+
+class SignInActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivitySignInBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        val btn = findViewById<Button>(R.id.signInAppCompatButton)
+
+        btn.setOnClickListener {
+            val mEmail = binding.emailEditText.text.toString()
+            val mPassword = binding.passwordEditText.text.toString()
+
+            when{
+                mEmail.isEmpty() || mPassword.isEmpty()->{
+                    Toast.makeText(baseContext, "Mail o contraseña incorrecta. ",
+                    Toast.LENGTH_SHORT).show()
+                }else->{
+                    SignIn(mEmail, mPassword)
+                }
+            }
+        }
+
+    }
+
+    private fun SignIn(email:String, password:String){
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    //val user = auth.currentUser
+                    //updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    //updateUI(null)
+                }
+            }
+    }
+}
